69583d964516979ffd10df22fef4fe0924a360c6
Imran <smdimran838@gmail.com>
2025-01-08 11:37:14 +0530

    Changed audio

--- app/src/main/java/aidev/com/salahtimer/view/QuranChapterAdapter.java
+++ app/src/main/java/aidev/com/salahtimer/view/QuranChapterAdapter.java
@@ -272,14 +272,14 @@ public class QuranChapterAdapter extends RecyclerView.Adapter<QuranChapterAdapte
         if(num==1)
         {
 
-            url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/00100"+ (position+2) + ".mp3";
+            url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/00100"+ (position+2) + ".mp3";
 
         }
         else if(num>1&&num<10)
         {
             temp = "00"+num+"000";
             start = Integer.parseInt(temp);
-            url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/00"+ (start+position+1) + ".mp3";
+            url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/00"+ (start+position+1) + ".mp3";
 
         }
         else if(num == 17){
@@ -288,13 +288,13 @@ public class QuranChapterAdapter extends RecyclerView.Adapter<QuranChapterAdapte
             if((position+1)==1 || (position+1)==2){
                 temp = "0" + num+ "000";
                 start = Integer.parseInt(temp);
-                url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/0"+ (start+position+1) + ".mp3";
+                url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/0"+ (start+position+1) + ".mp3";
 
             }
             if((position+1)>=3){
                 temp = "0" + num+ "000";
                 start = Integer.parseInt(temp);
-                url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/0"+ (start+position) + ".mp3";
+                url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/0"+ (start+position) + ".mp3";
 
             }
         }
@@ -302,14 +302,14 @@ public class QuranChapterAdapter extends RecyclerView.Adapter<QuranChapterAdapte
         {
             temp = "0" + num+ "000";
             start = Integer.parseInt(temp);
-            url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/0"+ (start+position+1) + ".mp3";
+            url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/0"+ (start+position+1) + ".mp3";
 
         }
         else if (num>=100 && num<115)
         {
             temp = num+"000";
             start = Integer.parseInt(temp);
-            url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/"+ (start+position+1) + ".mp3";
+            url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/"+ (start+position+1) + ".mp3";
 
         }
 
